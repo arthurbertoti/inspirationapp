6eba46cd6e0bb59ac5e4c66378957563f8be6a34
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-12 15:37:46 -0300

    feat(homeScreen): use AdviceComponent and getAdviceById, create handleCopy function error message

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,65 @@
-import { useEffect, useState } from "react"
-import { Text, View } from "react-native"
-import { getAdvice } from "@/services"
-import { Advice } from "@/models"
+import { useCallback, useState } from "react"
+import { Alert, Text, View } from "react-native"
+import { useFocusEffect } from "expo-router"
+import * as Clipboard from "expo-clipboard"
 import { AxiosError } from "axios"
 
-export default function HomeScreen() {
-  const [data, setData] = useState<Advice>()
-  const [error, setError] = useState<AxiosError>()
+import { getAdviceById } from "@/services"
 
-  const get = async () => {
-    const [response, error] = await getAdvice(1)
-    if (error) {
-      setError(error)
-      return
+import { Advice, ErrorMessage } from "@/models"
+
+import { AdviceComponent, LoadingComponent } from "@/components"
+
+export default function HomeScreen() {
+  const [dailyAdvice, setDailyAdvice] = useState<Advice | null>(null)
+  const [error, setError] = useState<AxiosError | ErrorMessage | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  const getDailyAdvice = async () => {
+    try {
+      setDailyAdvice(null)
+      setError(null)
+      setLoading(true)
+      const [response, error, callError] = await getAdviceById(4)
+      if (error) {
+        setError(error)
+        return
+      }
+      setDailyAdvice(response)
+    } catch (error) {
+      setError(error as AxiosError)
+    } finally {
+      setLoading(false)
     }
-    setData(response)
   }
 
-  useEffect(() => {
-    get()
-  }, [])
+  const handleCopy = async (text: string) => {
+    try {
+      await Clipboard.setStringAsync(text)
+      Alert.alert("Message copied!", `"${text}"`)
+    } catch (error) {
+      Alert.alert("Error!", "The message could not be copied!")
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      getDailyAdvice()
+    }, [])
+  )
 
   return (
-    <View className="flex flex-1 items-center justify-center content-center">
-      <Text className="border border-black">Hello World!</Text>
-      <Text className="border border-black">
-        {data ? data.slip.advice : "nada3"}
-      </Text>
+    <View className="flex flex-1 items-center justify-center content-center gap-5">
+      {loading ? (
+        <LoadingComponent />
+      ) : dailyAdvice?.slip && !error ? (
+        <AdviceComponent
+          advice={dailyAdvice}
+          copyToClipboard={() => handleCopy(dailyAdvice.slip.advice)}
+        />
+      ) : (
+        <Text>{"An error occurred! Try again restarting the app!"}</Text>
+      )}
     </View>
   )
 }
