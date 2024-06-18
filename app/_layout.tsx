af4950672e66f3ad03a10d34bd7459cb07c8a1f7
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-18 14:57:18 -0300

    chore: add one signal and expo dev client packages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,14 @@ import { Stack } from "expo-router"
 
 import "../global.css"
 
+import { LogLevel, OneSignal } from "react-native-onesignal"
+import Constants from "expo-constants"
+
+OneSignal.Debug.setLogLevel(LogLevel.Verbose)
+OneSignal.initialize(Constants.expoConfig.extra.oneSignalAppId)
+
+OneSignal.Notifications.requestPermission(true)
+
 export default function RootLayout() {
   return (
     <Stack>
