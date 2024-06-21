d82092702444e4bea4da56db15fa3e1b370d3cd9
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-21 15:58:41 -0300

    feat(notifications): create and use getUserTags

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,11 +1,60 @@
-import { Text } from "react-native"
+import { useCallback, useState } from "react"
+import { SafeAreaView, Switch, TextInput, View } from "react-native"
 
 import { DefaultScreen } from "@/components"
+import { OneSignal } from "react-native-onesignal"
+import { useFocusEffect } from "expo-router"
 
 export default function notificationsScreen() {
+  const [morningAdvice, setMorningAdvice] = useState("")
+  const [eveningAdvice, setEveningAdvice] = useState("")
+  const [morningNotification, setMorningNotification] = useState(false)
+  const [eveningNotification, setEveningNotification] = useState(false)
+
+  const getUserTags = async () => {
+    try {
+      const tags = await OneSignal.User.getTags()
+      setMorningAdvice(tags.morning_advice)
+      setEveningAdvice(tags.evening_advice)
+    } catch (error) {
+      console.error("Error fetching tags:", error)
+    }
+  }
+  useFocusEffect(
+    useCallback(() => {
+      getUserTags()
+    }, [])
+  )
   return (
     <DefaultScreen>
-      <Text>aham</Text>
+      <SafeAreaView>
+        <View>
+          <Switch
+            value={morningNotification}
+            onValueChange={setMorningNotification}
+          />
+          {morningNotification && (
+            <TextInput
+              onChangeText={setMorningAdvice}
+              value={morningAdvice}
+              placeholder="morning notification"
+            />
+          )}
+        </View>
+        <View>
+          <Switch
+            value={eveningNotification}
+            onValueChange={setEveningNotification}
+          />
+          {eveningNotification && (
+            <TextInput
+              onChangeText={setEveningAdvice}
+              value={eveningAdvice}
+              placeholder="evening notification"
+            />
+          )}
+        </View>
+      </SafeAreaView>
     </DefaultScreen>
   )
 }
