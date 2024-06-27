885fc81b0ca39d70ca4f5f47da4a2c3bfc853d11
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-27 15:19:01 -0300

    style(random screen): place randomButton in the bottom and center the advice

--- app/(tabs)/random.tsx
+++ app/(tabs)/random.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react"
-import { Text, Button, TouchableOpacity } from "react-native"
+import { Text, Button, TouchableOpacity, View } from "react-native"
 import { useFocusEffect } from "expo-router"
 import { AxiosError } from "axios"
 
@@ -38,33 +38,37 @@ export default function RandomScreen() {
 
   return (
     <DefaultScreen>
-      <TouchableOpacity
-        className="bg-primary p-4 rounded-lg mb-4"
-        disabled={loading}
-        onPress={() => handleRandomAdvice()}
-      >
-        <Text className="text-white text-lg">
-          Press to take another random advice!
-        </Text>
-      </TouchableOpacity>
-      {loading ? (
-        <LoadingComponent />
-      ) : error ? (
-        <Text className="text-red-500">
-          An error occurred! Try again restarting the app!
-        </Text>
-      ) : randomAdvice ? (
-        <AdviceComponent
-          advice={randomAdvice}
-          copyToClipboard
-          favoriteAdvice={() => handleFavorite(randomAdvice)}
-          shareContent
-        />
-      ) : (
-        errorMessage && (
-          <Text className="text-red-500">{errorMessage.message.text}</Text>
-        )
-      )}
+      <View className="flex-1 justify-end w-full">
+        <View className="h-3/5 flex items-center justify-between content-center gap-3 w-full">
+          {loading ? (
+            <LoadingComponent />
+          ) : error ? (
+            <Text className="text-red-500">
+              An error occurred! Try again restarting the app!
+            </Text>
+          ) : randomAdvice ? (
+            <AdviceComponent
+              advice={randomAdvice}
+              copyToClipboard
+              favoriteAdvice={() => handleFavorite(randomAdvice)}
+              shareContent
+            />
+          ) : (
+            errorMessage && (
+              <Text className="text-red-500">{errorMessage.message.text}</Text>
+            )
+          )}
+          <TouchableOpacity
+            className="bg-primary justify-self-end p-4 rounded-lg mb-4 w-4/5"
+            disabled={loading}
+            onPress={() => handleRandomAdvice()}
+          >
+            <Text className="text-white text-lg">
+              Press to take another random advice!
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </DefaultScreen>
   )
 }
