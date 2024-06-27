dfc810277a821e6f432c3517ff6b26dee94a5e76
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-27 17:00:41 -0300

    feat(notifications screen): enable userTags CRUD functions to user

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,57 +1,195 @@
-import { useCallback, useState } from "react"
-import { SafeAreaView, Switch, TextInput, View } from "react-native"
-
-import { DefaultScreen } from "@/components"
-import { OneSignal } from "react-native-onesignal"
+import { useCallback, useRef, useState } from "react"
 import { useFocusEffect } from "expo-router"
+import { OneSignal } from "react-native-onesignal"
+import clsx from "clsx"
+
+import {
+  Button,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native"
+
+import {
+  createEveningAdviceTag,
+  removeUserTag,
+  createMorningAdviceTag,
+} from "@/services"
+
+import { DefaultScreen, EditNotification, LoadingComponent } from "@/components"
+
+type Advices = {
+  morning: string | null
+  evening: string | null
+}
 
 export default function notificationsScreen() {
-  const [morningAdvice, setMorningAdvice] = useState("")
-  const [eveningAdvice, setEveningAdvice] = useState("")
-  const [morningNotification, setMorningNotification] = useState(false)
-  const [eveningNotification, setEveningNotification] = useState(false)
+  const [advices, setAdvices] = useState<Advices>({
+    morning: null,
+    evening: null,
+  })
 
-  const getUserTags = async () => {
+  const [notification, setNotification] = useState<{
+    morning: boolean
+    evening: boolean
+  }>({
+    morning: false,
+    evening: false,
+  })
+  const [editing, setEditing] = useState(false)
+
+  const morningAdvice = useRef<TextInput>(null)
+  const eveningAdvice = useRef<TextInput>(null)
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleGetUserTags = async () => {
+    setLoading(true)
     try {
       const tags = await OneSignal.User.getTags()
-      setMorningAdvice(tags.morning_advice)
-      setEveningAdvice(tags.evening_advice)
+      setNotification({
+        morning: Boolean(tags.morning_advice),
+        evening: Boolean(tags.evening_advice),
+      })
+      setAdvices({
+        morning: tags.morning_advice ? tags.morning_advice : null,
+        evening: tags.evening_advice ? tags.evening_advice : null,
+      })
     } catch (error) {
       console.error("Error fetching tags:", error)
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleCancelEdit = async () => {
+    setEditing(!editing)
+  }
+
+  const handleEdit = () => {
+    setEditing(!editing)
+  }
+
+  const handleSaveUserTags = async () => {
+    try {
+      setEditing(false)
+      setLoading(true)
+
+      if (
+        advices.evening !== null &&
+        advices.evening !== "" &&
+        notification.evening === true
+      ) {
+        await createEveningAdviceTag({ text: advices.evening })
+      } else removeUserTag("evening_advice")
+
+      if (
+        advices.morning !== null &&
+        advices.morning !== "" &&
+        notification.morning === true
+      ) {
+        await createMorningAdviceTag({ text: advices.morning })
+      } else removeUserTag("morning_advice")
+    } catch (error) {
+      console.error("Error creating tags:", error)
+      setError(true)
+    } finally {
+      setLoading(false)
     }
   }
   useFocusEffect(
     useCallback(() => {
-      getUserTags()
+      handleGetUserTags()
     }, [])
   )
   return (
     <DefaultScreen>
-      <SafeAreaView>
-        <View>
-          <Switch
-            value={morningNotification}
-            onValueChange={setMorningNotification}
-          />
-          {morningNotification && (
-            <TextInput
-              onChangeText={setMorningAdvice}
-              value={morningAdvice}
-              placeholder="morning notification"
-            />
-          )}
-        </View>
-        <View>
-          <Switch
-            value={eveningNotification}
-            onValueChange={setEveningNotification}
-          />
-          {eveningNotification && (
-            <TextInput
-              onChangeText={setEveningAdvice}
-              value={eveningAdvice}
-              placeholder="evening notification"
-            />
+      <SafeAreaView className="w-full h-full justify-end">
+        <View className="h-3/4 w-full p-3">
+          {loading ? (
+            <LoadingComponent />
+          ) : error ? (
+            <View className="flex-1 pt-10 justify-between">
+              <Text className="text-red-500 text-2xl font-bold text-center">
+                Error loading settings!
+              </Text>
+              <Button
+                title="Try again"
+                onPress={handleGetUserTags}
+                color={"red"}
+              />
+            </View>
+          ) : (
+            <View className="flex-1 justify-between ">
+              <View className="flex gap-4">
+                <EditNotification
+                  title="Morning notification"
+                  inputRef={morningAdvice}
+                  onChangeText={(text: string) =>
+                    setAdvices({ ...advices, morning: text })
+                  }
+                  textValue={advices.morning ? advices.morning : ""}
+                  placeholder="Type something nice for you!"
+                  editable={editing}
+                  onToggle={() =>
+                    setNotification({
+                      ...notification,
+                      morning: !notification.morning,
+                    })
+                  }
+                  toggleValue={notification.morning}
+                  toggleDisabled={!editing}
+                />
+                <EditNotification
+                  title="Evening notification"
+                  inputRef={eveningAdvice}
+                  onChangeText={(text: string) =>
+                    setAdvices({ ...advices, evening: text })
+                  }
+                  textValue={advices.evening ? advices.evening : ""}
+                  placeholder="Type something nice for you!"
+                  editable={editing}
+                  onToggle={() =>
+                    setNotification({
+                      ...notification,
+                      evening: !notification.evening,
+                    })
+                  }
+                  toggleValue={notification.evening}
+                  toggleDisabled={!editing}
+                />
+              </View>
+              <View className="flex-row w-full justify-around items-center gap-2">
+                <TouchableOpacity
+                  className={clsx(
+                    "bg-primary justify-self-end p-4 rounded-lg mb-1",
+                    editing ? "w-2/5" : "w-4/5"
+                  )}
+                  onPress={() => (editing ? handleCancelEdit() : handleEdit())}
+                >
+                  <Text className="text-white text-2xl font-bold text-center">
+                    {editing ? "Cancel" : "Edit"}
+                  </Text>
+                </TouchableOpacity>
+                {editing && (
+                  <TouchableOpacity
+                    className={clsx(
+                      "bg-primary justify-self-end p-4 rounded-lg mb-1",
+                      editing ? "w-2/5" : "w-4/5"
+                    )}
+                    onPress={handleSaveUserTags}
+                  >
+                    <Text className="text-white text-2xl font-bold text-center">
+                      Save
+                    </Text>
+                  </TouchableOpacity>
+                )}
+              </View>
+            </View>
           )}
         </View>
       </SafeAreaView>
