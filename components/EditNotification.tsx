b6da47e7212b4c0ce47a64761089cbe5183680a2
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-27 16:52:28 -0300

    feat(EditNotification): create component

---
+++ components/EditNotification.tsx
@@ -0,0 +1,47 @@
+import { RefObject } from "react"
+import { Button, Switch, Text, TextInput, View } from "react-native"
+
+type EditNotificationProps = {
+  title: string
+  inputRef: RefObject<TextInput>
+  onChangeText: (text: string) => void
+  textValue: string
+  placeholder: string
+  editable: boolean
+  onToggle: () => void
+  toggleValue: boolean
+  toggleDisabled: boolean
+}
+export const EditNotification = ({
+  title,
+  inputRef,
+  onChangeText,
+  textValue,
+  placeholder,
+  editable,
+  onToggle,
+  toggleValue,
+  toggleDisabled,
+}: EditNotificationProps) => {
+  return (
+    <View className="flex-row w-full justify-between gap-2 px-4">
+      <View>
+        <Text>{title}</Text>
+        <TextInput
+          ref={inputRef}
+          onChangeText={onChangeText}
+          value={textValue}
+          placeholder={placeholder}
+          editable={editable}
+          autoCorrect={false}
+          returnKeyType="done"
+        />
+      </View>
+      <Switch
+        value={toggleValue}
+        onChange={onToggle}
+        disabled={toggleDisabled}
+      />
+    </View>
+  )
+}
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export * from "./Advice"
 export * from "./DefaultScreen"
+export * from "./EditNotification"
 export * from "./Loading"
